44a2d4b1562bcf7785103428cd1e40935a82a84f
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2022-04-04 23:34:23 +0300

    refactoring: extract wav_to_frequencies function

--- wavtobeep.py
+++ wavtobeep.py
@@ -15,6 +15,9 @@ import subprocess
 import numpy as np
 from scipy.io import wavfile
 
+# Maximum permitted length of the processed data (in seconds)
+MAX_LEN = 40
+
 
 def frequency(note, octave):
     """http://latecladeescape.com/t/Frecuencia+de+las+notas+musicales"""
@@ -23,6 +26,53 @@ def frequency(note, octave):
 
 # Accepted frequencies
 FREQS = np.array([1] + [frequency(n, o) for o in range(10) for n in range(1, 13)])
+
+
+def wav_to_frequencies(filename: str, window: int) -> list:
+    """Convert WAV to a list of frequencies
+
+    Args:
+        filename (str): WAV filename
+        window (int): resolution of the time window for frequency analysis in miliseconds
+
+    Returns:
+        list of (duration, frequency) tuples
+    """
+
+    fs, data = wavfile.read(filename)
+    data = data[: MAX_LEN * fs]
+    # Window size and overlap definition for the spectral analysis
+    w = int(fs / 1000 * window)
+    overlap = w // 2
+    # Number of chunks with the selected parameters
+    n = len(data) // (w - overlap)
+    # Duration of each beep
+    dur = int(1000 * (len(data) / float(fs)) / n)
+    # Array of frequencies
+    freq = np.arange(0, fs / 2, (fs / 2) / (w / 2))
+    # We truncate the data array to be a multiplo of the step
+    data = data[: n * (w - overlap)]
+    blw = np.blackman(w)
+    freql = []
+    for i in range(0, len(data), w - overlap):
+        chunk = data[i : i + w]
+        if len(chunk) != w:
+            chunk = chunk.copy()
+            chunk.resize(w)
+        chunk = chunk * blw
+        chunk = chunk - np.mean(chunk)
+        ft = np.fft.fft(chunk)
+        ft = ft[: w // 2]
+        # Get the most representative frequency of the chunk
+        hz = freq[np.absolute(ft).argmax()]
+        # We check the frequency table to get the closest
+        hz = FREQS[np.abs(FREQS - hz).argmin()]
+        if freql and freql[-1][1] == hz:
+            freql[-1] = (freql[-1][0] + dur, hz)
+        else:
+            freql.append((dur, hz))
+    return freql
+
 
 # Argument parsing
 parser = argparse.ArgumentParser(
@@ -43,42 +93,8 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-# Maximum permitted length of the processed data (in seconds)
-MAX_LEN = 40
-# Resolution of the time window for frequency analysis.
-CH_MS = args.window
-fs, data = wavfile.read(args.file)
-data = data[: MAX_LEN * fs]
-# Window size and overlap definition for the spectral analysis
-w = int(fs / 1000 * CH_MS)
-overlap = w // 2
-# Number of chunks with the selected parameters
-n = len(data) // (w - overlap)
-# Duration of each beep
-dur = int(1000 * (len(data) / float(fs)) / n)
-# Array of frequencies
-freq = np.arange(0, fs / 2, (fs / 2) / (w / 2))
-# We truncate the data array to be a multiplo of the step
-data = data[: n * (w - overlap)]
-blw = np.blackman(w)
-freql = []
-for i in range(0, len(data), w - overlap):
-    chunk = data[i : i + w]
-    if len(chunk) != w:
-        chunk = chunk.copy()
-        chunk.resize(w)
-    chunk = chunk * blw
-    chunk = chunk - np.mean(chunk)
-    ft = np.fft.fft(chunk)
-    ft = ft[: w // 2]
-    # Get the most representative frequency of the chunk
-    hz = freq[np.absolute(ft).argmax()]
-    # We check the frequency table to get the closest
-    hz = FREQS[np.abs(FREQS - hz).argmin()]
-    if freql and freql[-1][1] == hz:
-        freql[-1] = (freql[-1][0] + dur, hz)
-    else:
-        freql.append((dur, hz))
+freql = wav_to_frequencies(args.file, args.window)
+
 # Command build (beep version)
 # Example: beep -l 50 -f 140.0 -n -l 250 -f 370.0 -n -l 50 -f 190.0
 cmd = ['beep']
